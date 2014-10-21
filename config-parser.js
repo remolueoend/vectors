05747fd0038d33ac33467d2ff66700c71070ecc8
Remo Lüönd <remo.zumsteg@outlook.com>
2014-10-21 14:33:09 +0200

    changed config format
    replaced "-" through "_"

--- config-parser.js
+++ config-parser.js
@@ -1,9 +1,3 @@
-/**
-* Sample Config:
-* v0   : LENGTH_V1,ROT_V1,COLOR_V1;LENGTH_V2,ROT_V2,COLOR_V2;
-* > v0 : vVERSION-LENGTH_V1,ROT_V1,COLOR_V1;LENGTH_V2,ROT_V2,COLOR_V2;
-*/
-
 
 /**
 * This namespace provides functions for handling app configurations.
@@ -22,7 +16,7 @@ vectors.config = {
 	* containing the config objects for the vectors.
 	*/
     parse: function(str){
-    	var verSplt = str.split("-"),
+    	var verSplt = str.split("_"),
     		ver = verSplt.length > 1 ? verSplt[0] : "v0";
     	if(typeof vectors.config.vectorValueParsers[ver] !== "function") throw "Invalid config version";
 
@@ -46,7 +40,7 @@ vectors.config = {
                 res += ";";
             }
         }
-        return vectors.config.currentVersion + "-" + res;
+        return vectors.config.currentVersion + "_" + res;
     },
 
     /*
