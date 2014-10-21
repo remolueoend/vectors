74354046ed428d4e36b1d18bf9c893a6e9718f67
Remo Lüönd <remo.zumsteg@outlook.com>
2014-10-21 14:11:11 +0200

    Config versions
    Implemented config versions and version based parsers

--- config-parser.js
+++ config-parser.js
@@ -1,17 +1,39 @@
+/**
+* Sample Config:
+* v0   : LENGTH_V1,ROT_V1,COLOR_V1;LENGTH_V2,ROT_V2,COLOR_V2;
+* > v0 : vVERSION-LENGTH_V1,ROT_V1,COLOR_V1;LENGTH_V2,ROT_V2,COLOR_V2;
+*/
 
+
+/**
+* This namespace provides functions for handling app configurations.
+* Config versions conmpatibility:
+* v0: v0
+* v1: v1, v0
+*/
 vectors.config = {
+	/**
+	* Contains the current configuration version
+	*/
+	currentVersion: "v1",
+
+	/**
+	* Parses a configuration string and returns an array
+	* containing the config objects for the vectors.
+	*/
     parse: function(str){
-        var vs = str.split(";"),
+    	var verSplt = str.split("-"),
+    		ver = verSplt.length > 1 ? verSplt[0] : "v0";
+    	if(typeof vectors.config.vectorValueParsers[ver] !== "function") throw "Invalid config version";
+
+        var vs = verSplt[verSplt.length - 1].split(";"),
             res = [];
+        if(!vs.length || !vs[0]) return res;
+
         for(var i = 0; i < vs.length; i++){
-            var ps = vs[i].split(",");
-            if(ps.length !== 3){ throw "Invalid config"; }
-            res.push({
-                l: parseFloat(ps[0]),
-                r: parseFloat(ps[1]),
-                c: ps[2]
-            });
+            res.push(vectors.config.vectorValueParsers[ver](vs[i]));
         }
+
         return res;
     },
 
@@ -24,6 +46,30 @@ vectors.config = {
                 res += ";";
             }
         }
-        return res;
+        return vectors.config.currentVersion + "-" + res;
+    },
+
+    /*
+    * Object containing a parser for every available config version.
+    */
+    vectorValueParsers: {
+    	v0: function(vStr){
+    		var ps = vStr.split(",");
+    		if(ps.length !== 3) throw "Invalid config";
+    		return {
+                l: parseFloat(ps[0]),
+                r: parseFloat(ps[1]) * 100,
+                c: ps[2]
+            };
+    	},
+    	v1: function(vStr){
+    		var ps = vStr.split(",");
+    		if(ps.length !== 3) throw "Invalid config";
+    		return {
+                l: parseFloat(ps[0]),
+                r: parseFloat(ps[1]),
+                c: ps[2]
+            };
+    	}
     }
 };
