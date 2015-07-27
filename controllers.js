9d8bb043c793a2ee4809a7c8b8eb2b4258f800ca
Remo Zumsteg <remo.zumsteg@outlook.com>
2015-07-27 07:23:02 +0000

    code refactoring

--- controllers.js
+++ controllers.js
@@ -7,70 +7,70 @@
 */
 
 (function(app){
-    
+
     'use strict';
-    
+
     app.controller('appCtrl', ['$scope', 'drawSrv', 'config', function($scope, drawSrv, config){
         var startAngle = -1 * (90 * (Math.PI / 180)),
             img = document.getElementById("image"),
             drawSrv;
-    
+
         function resizeCanvas(canv){
             canv.elem.setAttribute("width", window.innerWidth);
             canv.elem.setAttribute("height", window.innerHeight - 60);
         }
-    
+
         function updateImage(){
             img.src = canv.elem.toDataURL();
         }
-    
+
         function Vector(length, rotation, color){
             this.l = length;
             this.r = rotation;
             this.c = color;
             this.a = startAngle;
-    
+
         }
         Vector.prototype = {
             getEndPoint: function(startPoint, canvas){
                 var base = canvas.getHeight() / 100;
                 var dy = Math.sin(this.a) * base * this.l;
                 var dx = Math.cos(this.a) * base * this.l;
-    
+
                 return {
                     x: startPoint.x + dx,
                     y: startPoint.y + dy
                 }
             },
-    
+
             rotate: function(){
                 this.a += this.r / 100;
             },
-    
+
             compare: function(v){
                 return this.l === v.l &&
                     this.r === v.r &&
                     this.c === v.c;
             }
         };
-    
+
         function Canvas(canvas){
             this.elem = canvas;
         }
         Canvas.prototype = {
-    
+
             clear: function(){
                 var s = this.getSize();
                 this.getContext().clearRect(0, 0, s.w, s.h);
             },
-    
+
             getSize: function(){
                 return {
                     w: this.elem.offsetWidth,
                     h: this.elem.offsetHeight
                 };
             },
-    
+
             getCenter: function(){
                 var s = this.getSize();
                 return {
@@ -78,40 +78,40 @@
                     y: s.h / 2
                 };
             },
-    
+
             getContext: function(){
                 return this.elem.getContext("2d");
             },
-    
+
             getHeight: function(){
                 return this.elem.height;
             }
         };
-    
+
         $scope.vectors = [];
         $scope.config = "";
         $scope.points = 0;
-    
+
         $scope.addVector = function(){
             $scope.vectors.push(new Vector(20, 1, "blue"));
         };
-    
+
         $scope.deleteVector = function(index){
             $scope.vectors.splice(index, 1);
         };
-    
+
         $scope.clearPoints = function(){
             drawSrv.clear();
             for(var i = 0; i < $scope.vectors.length; i++){
                 $scope.vectors[i].a = startAngle
             }
         };
-    
+
         $scope.toggleConfig = function(){
             $scope.showSettings = ($scope.showSettings == null || $scope.showSettings === false);
             if($scope.showImage){ $scope.showImage = false; }
         };
-    
+
         $scope.toggleImage = function(){
             $scope.showImage = ($scope.showImage == null || $scope.showImage === false);
             if($scope.showSettings){ $scope.showSettings = false; }
@@ -119,7 +119,7 @@
                 updateImage();
             }
         };
-    
+
         $scope.loadConfig = function(){
             var c;
             try{
@@ -137,7 +137,7 @@
                 $scope.vectors.push(v);
             }
         };
-    
+
         $scope.$watch("vectors", function(n, o){
             if(n.length !== o.length || !n.every(function(v, i){
                 return v.compare(o[i]); }
@@ -145,23 +145,23 @@
                 $scope.config = config.stringify($scope.vectors);
             }
         }, true);
-    
+
         var canv = new Canvas(document.getElementById("canvas"));
-    
+
         window.addEventListener("resize", function(){
             resizeCanvas(canv);
         });
         resizeCanvas(canv);
-    
+
         drawSrv = new drawSrv($scope, canv)
         drawSrv.on("drewPoints", function(pointCount){
             $scope.$apply(function(){
                 $scope.points = pointCount;
             });
         });
-    
+
         drawSrv.draw();
-    
+
     }]);
 
 })(window.vectors.app);
